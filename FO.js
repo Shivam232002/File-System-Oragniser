c29a5d35830af59b3c478b48dfe430f4c4b5f67a
SHIVAM GULATI <gulatishivam.23@gmail.com>
2021-12-04 16:44:13 +0530

    without tree function

--- FO.js
+++ FO.js
@@ -24,7 +24,7 @@ let inputArr = process.argv.slice(2); // slice is used to extart the commands an
 let command = inputArr[0]; // organzie , help . tree , default
 
 let types = {
-    media: ["mp4", "mkv", "mp3"],
+    media: ["mp4", "mkv", "mp3", "jpg"],
     archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
     documents: [
         "docx",
@@ -39,7 +39,7 @@ let types = {
         "odf",
         "txt",
         "ps",
-        "tex",
+        "tex"
     ],
     app: ["exe", "dmg", "pkg", "deb"],
 };
@@ -61,64 +61,84 @@ switch (command) {
 
 function treeFn() {
     console.log("Tree Function Implemented");
+    
 }
 
 function organizeFn(dirpath) {
     let destPath;
-    if(dirpath == undefined){
+    if (dirpath == undefined) {
         console.log("kindly enter the path")
         return;
     }
-    else{
+    else {
         let doesExist = fs.existsSync(dirpath);
-        if(doesExist){
-            destPath = path.join(dirpath,"organised_files");
-           if(fs.existsSync(destPath) == false){
-           fs.mkdirSync(destPath);}
+        if (doesExist) {
+            destPath = path.join(dirpath, "organised_files");
+            if (fs.existsSync(destPath) == false) {
+                fs.mkdirSync(destPath);
+            }
         }
-        else{
+        else {
             console.log("kindly enter the correct path")
             return;
 
         }
     }
-    organiseHelper(dirpath , destPath);   
+    organiseHelper(dirpath, destPath);
 }
-function organiseHelper(src,dest){
-    let childName=fs.readdirSync(src);
+function organiseHelper(src, dest) {
+    let childName = fs.readdirSync(src);
     // console.log(childName)
-    for(let i = 0 ; i <childName.length ; i++){
-       let childAddress =  path.join(src,childName[i]);
-       let isfile =fs.lstatSync(childAddress).isFile();
-       if(isfile){
-        //    console.log(childName[i]);
-        let Category = getCategory(childName[i]);
-        console.log(childName[i],"belongs to --> ",Category);
-       } 
-    }
+    for (let i = 0; i < childName.length; i++) {
+        let childAddress = path.join(src, childName[i]);
+        let isfile = fs.lstatSync(childAddress).isFile();
+        if (isfile) {
+            //    console.log(childName[i]);
+            let Category = getCategory(childName[i]);
+            console.log("Category of ",childName[i], " is ",Category );
+            // console.log(childName[i], "belongs to --> ", Category);
+            sendfiles(childAddress, dest, Category);
 
-    function getCategory(name){
-      let ext =  path.extname(name);
-        // console.log(ext);
-        ext = ext.slice(1);
-        for(let type in types){
-            let cTypeArray =types[type];
-            for(let i = 0 ; i <cTypeArray.length; i++){
-                if(ext=cTypeArray[i]){
-                    return type;
-                }
-                return "others";
-
-            }
         }
-
     }
 }
+function sendfiles(src,dest,Category){
+    let CategoryPath = path.join(dest,Category);
+    console.log(CategoryPath);
+    if(fs.existsSync(CategoryPath)== false){
+
+        fs.mkdirSync(CategoryPath);
+    }
+    let fileName =path.basename(src);
+    let destFilePath = path.join(CategoryPath,fileName);
+    fs.copyFileSync(src,destFilePath);
+    fs.unlinkSync(src);
+    console.log(fileName,"copied to ",Category);
+}
+
+function getCategory(name) {
+    let ext = path.extname(name);
+    // console.log(ext);
+    ext = ext.slice(1);
+    console.log("extension is ----> ", ext);
+    for (let type in types) {
+        let cTypeArray = types[type];
+        for (let i = 0; i < cTypeArray.length; i++) {
+            if (ext == cTypeArray[i]) {
+                return type;
+            }
+
+
+        }
+    }
+    return "others";
+}
+
 function helpFn(dirpath) {
     console.log(`List of all the commands -
     1)Tree Command - node FO.js tree <dirName>
     2) Organize- node FO.js organize <dirName
-    3) Help - node FO.js help `); 
+    3) Help - node FO.js help `);
 }
 
 
